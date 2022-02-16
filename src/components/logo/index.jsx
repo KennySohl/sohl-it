57d3c4352a716d48ee67aa3de88cd958bfd9e548
KennySohl <kenny.sohl@gmail.com>
2022-02-16 20:00:07 +0100

    Added Navbar and Logo

---
+++ src/components/logo/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+import SohlitLogoImg from "../../assets/logo_v4.png";
+
+const LogoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const LogoImg = styled.div`
+    width: 43px;
+    height 37px;
+
+    img {
+        width: 100%;
+        height: 100%;
+    }
+`;
+
+const LogoText = styled.h2`
+  font-size: 16px;
+  margin: 0;
+  margin-left: 4px;
+  color: #222;
+  font-weight: 500;
+`;
+
+export function Logo(props) {
+  return (
+    <LogoWrapper>
+      <LogoImg>
+        <img src={SohlitLogoImg} alt="Sohl.it Logo" />
+      </LogoImg>
+      <LogoText>Sohl.it</LogoText>
+    </LogoWrapper>
+  );
+}
---
+++ src/components/navbar/navLinks.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import styled from "styled-components"
+
+const NavLinksContainer = styled.div`
+    height: 100%;
+    display: flex;
+    align-items: center;
+`;
+
+const LinkWrapper = styled.ul`
+    margin: 0;
+    padding: 0;
+    display: flex;
+    height: 100%;
+    list-style: none;
+`;
+
+const LinkItem = styled.li`
+    height: 100%;
+    padding: 0 1.1em;
+    color: #222;
+    font-weight: 500;
+    font-size: 14px;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    border-top: 2px solid transparent;
+    transition: all 200ms ease-in-out;
+
+    &:hover {
+        border-top: 2px solid #004d98;     
+    }
+`;
+
+const Link = styled.a`
+    text-decoration: none;
+    color: inherit;
+    font-size: inherit;
+`;
+
+export function NavLinks(props) {
+        return <NavLinksContainer>
+            <LinkWrapper>
+                <LinkItem><Link href="#">About</Link></LinkItem>
+                <LinkItem><Link href="#">Work</Link></LinkItem>
+                <LinkItem><Link href="#">Contact</Link></LinkItem>
+                <LinkItem><Link href="#">Linkedin</Link></LinkItem>
+                <LinkItem><Link href="#">GitHub</Link></LinkItem>
+            </LinkWrapper>
+        </NavLinksContainer>
+}
